51b840638a045084d4a7957388ced982485a94f6
Syldris <hojeong6777@gmail.com>
2023-08-14 05:08:35 +0900

    진동 기능 추가

--- Assets/Setting/SettingScript.cs
+++ Assets/Setting/SettingScript.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingScript : MonoBehaviour
 {
     public GameObject SettingUI;
     public GameObject GameUI;
+    public Toggle Vibrate_Toggle;
+    public int VibrateInt;
+
+    private void Start()
+    {
+        VibrateInt = PlayerPrefs.GetInt("Vibrate");
+
+        if(VibrateInt == 0)
+        {
+            Vibrate_Toggle.isOn = true;
+        }
+        
+    }
 
     public void Setting_Window()
     {
@@ -17,5 +31,25 @@ public class SettingScript : MonoBehaviour
     {
         SettingUI.SetActive(false);
         GameUI.SetActive(true);
+    }
+
+    public void Vibrate()
+    {
+        if (Vibrate_Toggle.isOn)
+        {
+            Vibration.Vibrate(150); // Vibrate 150ms
+        }
+    }
+
+    public void Vibrate_save()
+    {
+        if(Vibrate_Toggle.isOn)
+        {
+            PlayerPrefs.SetInt("Vibrate", 0);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Vibrate", 1);
+        }
     }
 }
